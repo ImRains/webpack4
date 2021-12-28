861d04661852771278d54b3f3a60f9302c5292c0
qiuyu <958989376@qq.com>
2021-12-28 17:38:24 +0800

    增加sourceMap

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,12 @@ const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
     mode: 'development', // 选择打包模式 development，production
+    devtool:'cheap-module-eval-source-map', //传入source-map，即为开启。none为关闭 development模式下自动打开，报错会定位到开发代码，而不是压缩后的代码
+    // 最佳实践 开发 'cheap-module-eval-source-map' 生产 'cheap-module-source-map'
+    // sourcemap 相关文档 https://segmentfault.com/a/1190000008315937
+    // https://www.html5rocks.com/en/tutorials/developertools/sourcemaps/
+    // http://www.ruanyifeng.com/blog/2013/01/javascript_source_map.html
+    // https://www.youtube.com/watch?v=NkVes0UMe9Y
     entry: { // 入口文件
         main: './src/index.js' // 入口文件地址
     },
